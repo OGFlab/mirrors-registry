14d94adf2582f22675c38982eb23ae2b7a00a5de
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2024-01-05 14:22:59 +0800

    feat: rename and then don't process universitied data only

--- src/ogf_register_viewer/main.py
+++ src/ogf_register_viewer/main.py
@@ -6,18 +6,18 @@ from method.get_plain_dataframe import (
     get_plain_dataframe,
 )
 
-universities_dataframe: List[Dict] = get_plain_dataframe()
-universities_full: List[Dict] = copy.deepcopy(universities_dataframe)
-universities_completed: List[Dict] = list(
+elements_dataframe: List[Dict] = get_plain_dataframe()
+elements_full: List[Dict] = copy.deepcopy(elements_dataframe)
+elements_completed: List[Dict] = list(
     filter(
         bool,
         [
             item_dict if item_dict["short_name"] != "" else None
-            for item_dict in universities_dataframe
+            for item_dict in elements_dataframe
         ],
     )
 )
-universities_uncompleted: List[Dict] = list(
+elements_uncompleted: List[Dict] = list(
     filter(
         bool,
         [
@@ -25,57 +25,37 @@ universities_uncompleted: List[Dict] = list(
                 item_dict
                 if (item_dict["@type"], item_dict["@id"])
                 in list(
-                    set([(i["@type"], i["@id"]) for i in universities_full])
+                    set([(i["@type"], i["@id"]) for i in elements_full])
                     - set(
                         [
                             (i["@type"], i["@id"])
-                            for i in universities_completed
+                            for i in elements_completed
                         ]
                     )
                 )
                 else None
             )
-            for item_dict in universities_dataframe
+            for item_dict in elements_dataframe
         ],
     )
 )
 
-# # Power saving but not elegant in my eye
-# universities_uncompleted_temp: List[tuple] = list(
-#     set([(i["@type"], i["@id"]) for i in universities_full])
-#     - set([(i["@type"], i["@id"]) for i in universities_completed])
-# )
-# universities_uncompleted: List[Dict] = list(
-#     filter(
-#         bool,
-#         [
-#             (
-#                 item_dict
-#                 if (item_dict["@type"], item_dict["@id"])
-#                 in universities_uncompleted_temp
-#                 else None
-#             )
-#             for item_dict in universities_dataframe
-#         ],
-#     )
-# )
-
-universities_completed_sorted = sorted(
-    universities_completed,
+elements_completed_sorted = sorted(
+    elements_completed,
     key=lambda x: (x["addr:province"], x["short_name"], x["@id"]),
     reverse=True,
 )
-universities_uncompleted_sorted = sorted(
-    universities_uncompleted, key=lambda x: x["@id"]
+elements_uncompleted_sorted = sorted(
+    elements_uncompleted, key=lambda x: x["@id"]
 )
 # from pprint import pprint
 # pprint(universities_full)
 print(
-    len(universities_completed_sorted),
+    len(elements_completed_sorted),
     "+",
-    len(universities_uncompleted_sorted),
+    len(elements_uncompleted_sorted),
     "=",
-    len(universities_full),
+    len(elements_full),
 )
 
 # TODO
@@ -83,4 +63,4 @@ print(
 # 2. 自动识别母体大学，合并多个校区代码不会重复出现
 # 3. 在英文名中自动识别简称可能来自的字母（首字母大写）并给出加粗该字母的建议。（可能比较困难）
 
-gen_pages(universities_completed_sorted, universities_uncompleted_sorted)
+gen_pages(elements_completed_sorted, elements_uncompleted_sorted)
