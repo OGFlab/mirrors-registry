28a3b8a447ec34d4684aa4553e3f364f573329dd
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2024-02-15 20:45:05 +0800

    refactor!: to allow batch query, make variable functional

--- src/ogf_register_viewer/main.py
+++ src/ogf_register_viewer/main.py
@@ -7,69 +7,98 @@ from method.get_plain_dataframe import (
     get_plain_dataframe,
 )
 
-# profile_name = "registry.moe.gov.hx.json"
-profile_name = "airports.mot.gov.hx.json"
-profile_file = open("../assets/profile/" + profile_name, "r", encoding="utf-8")
-profile = json.loads(profile_file.read())
-profile_file.close()
 
-elements_dataframe: List[Dict] = get_plain_dataframe(
-    {
-        "key": profile["key"],
-        "type": profile["type"],
-        "poly": profile["poly"],
-    }
-)
-elements_full: List[Dict] = copy.deepcopy(elements_dataframe)
-elements_completed: List[Dict] = list(
-    filter(
-        bool,
-        [
-            item_dict if item_dict.get(profile["vital_key"]) != "" else None
-            for item_dict in elements_dataframe
-        ],
+MULTI_PROFILE_MODE = True
+
+
+def get_profile(profile_name: str) -> dict:
+    # profile_name = "registry.moe.gov.hx.json"
+    profile_name = "airports.mot.gov.hx.json"
+    profile_file = open(
+        "../assets/profile/" + profile_name, "r", encoding="utf-8"
     )
-)
-elements_uncompleted: List[Dict] = list(
-    filter(
-        bool,
-        [
-            (
+    profile = json.loads(profile_file.read())
+    profile_file.close()
+    return profile
+
+
+def elements_dataframe() -> List[Dict]:
+    return get_plain_dataframe(
+        {
+            "key": get_profile()["key"],
+            "type": get_profile()["type"],
+            "poly": get_profile()["poly"],
+        }
+    )
+
+
+def elements_full() -> List[Dict]:
+    return copy.deepcopy(elements_dataframe())
+
+
+def elements_completed() -> List[Dict]:
+    return list(
+        filter(
+            bool,
+            [
                 item_dict
-                if (item_dict["@type"], item_dict["@id"])
-                in list(
-                    set([(i["@type"], i["@id"]) for i in elements_full])
-                    - set([(i["@type"], i["@id"]) for i in elements_completed])
-                )
+                if item_dict.get(get_profile()["vital_key"]) != ""
                 else None
-            )
-            for item_dict in elements_dataframe
-        ],
+                for item_dict in elements_dataframe()
+            ],
+        )
     )
-)
 
-elements_completed_sorted = sorted(
-    elements_completed,
-    key=lambda x: (x.get("addr:province"), x.get("short_name"), x["@id"]),
-    reverse=True,
-)
-elements_uncompleted_sorted = sorted(
-    elements_uncompleted, key=lambda x: x["@id"]
-)
+
+def elements_uncompleted() -> List[Dict]:
+    return list(
+        filter(
+            bool,
+            [
+                (
+                    item_dict
+                    if (item_dict["@type"], item_dict["@id"])
+                    in list(
+                        set([(i["@type"], i["@id"]) for i in elements_full()])
+                        - set(
+                            [
+                                (i["@type"], i["@id"])
+                                for i in elements_completed()
+                            ]
+                        )
+                    )
+                    else None
+                )
+                for item_dict in elements_dataframe()
+            ],
+        )
+    )
+
+
+def elements_completed_sorted():
+    return sorted(
+        elements_completed(),
+        key=lambda x: (x.get("addr:province"), x.get("short_name"), x["@id"]),
+        reverse=True,
+    )
+
+
+def elements_uncompleted_sorted():
+    return sorted(elements_uncompleted(), key=lambda x: x["@id"])
+
 
 print(
-    len(elements_completed_sorted),
+    len(elements_completed_sorted()),
     "+",
-    len(elements_uncompleted_sorted),
+    len(elements_uncompleted_sorted()),
     "=",
-    len(elements_full),
+    len(elements_full()),
 )
-
 
 
 gen_pages(
-    elements_completed_sorted,
-    elements_uncompleted_sorted,
-    template_file_name=profile["template_file_name"],
-    optional_data={"page_title": profile["page_title"]},
+    elements_completed_sorted(),
+    elements_uncompleted_sorted(),
+    template_file_name=get_profile()["template_file_name"],
+    optional_data={"page_title": get_profile()["page_title"]},
 )
