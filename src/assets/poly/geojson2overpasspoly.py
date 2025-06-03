616e4a1e7d58f98143a101e5fed829458b9c3430
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2025-06-03 21:52:08 +0900

    feat: generate new poly file

--- src/assets/poly/geojson2overpasspoly.py
+++ src/assets/poly/geojson2overpasspoly.py
@@ -1,10 +1,24 @@
 import json
 
-source=input()
+source = input()
 
-with open(source,"r",encoding="utf-8") as f:
-    raw=json.loads(f.read())
+with open(source, "r", encoding="utf-8") as f:
+    raw = json.loads(f.read())
 
 # print(len(raw["features"][0]["geometry"]["coordinates"]))
 
-print([f"{round(i[1],6)} {round(i[0],6)}" for i in raw["features"][0]["geometry"]["coordinates"]])
+valid_content = (
+    '(poly:"\n'
+    + "\n".join(
+        [
+            f"{round(i[1],6)} {round(i[0],6)} "
+            for i in raw["features"][0]["geometry"]["coordinates"]
+        ]
+    )
+    + '\n")'
+)
+
+print(("=" * 15 + "\n") + valid_content)
+
+with open("output.overpassql_poly", "w", encoding="utf-8") as f:
+    f.write(valid_content)
